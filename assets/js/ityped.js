ab839b1f7145e0b46357c1da83f88412b4de380e
radoheritiana <radoheritiana.rhja.123@gmail.com>
2023-01-07 07:57:22 +0300

    move my_site to radoheritiana.github.io

---
+++ assets/js/ityped.js
@@ -0,0 +1,21 @@
+const pres = () => {
+  ityped.init("#pres", {
+    strings: [
+      "Courtois",
+      "Créatif",
+      "Curieux",
+      "Persévérant",
+      "Responsable",
+      "Motivé",
+    ],
+    typeSpeed: 80,
+    backSpeed: 30,
+    backDelay: 400,
+    startDelay: 400,
+    cursorChar: "|",
+    showCursor: true,
+    loop: true,
+  });
+};
+
+pres();
