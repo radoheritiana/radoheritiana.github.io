0453bf1cb6491991b3649bb834832c4667d2b2bd
radoheritiana <radoheritiana.rhja.123@gmail.com>
2024-04-13 21:30:14 +0300

    refont portfolio

---
+++ js/ityped.js
@@ -0,0 +1,18 @@
+const pres = () => {
+  ityped.init("#type", {
+    strings: [
+      "FullStack Developer",
+      "Web Developer",
+      "Mobile Developer",
+    ],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 500,
+    startDelay: 500,
+    cursorChar: "_",
+    showCursor: true,
+    loop: true,
+  });
+};
+
+pres();
