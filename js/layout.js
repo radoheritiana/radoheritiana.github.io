4eb2e42a917dc04bea84e14aa1598322b48b169e
radoheritiana <radoheritiana.rhja.123@gmail.com>
2025-11-21 19:18:45 +0300

    change delay on preloader : onload

--- js/layout.js
+++ js/layout.js
@@ -1,15 +1,4 @@
 var Layout = function () {
-    
-    // detect mobile device
-    var isMobileDevice = function() {
-        return  ((
-            navigator.userAgent.match(/Android/i) ||
-            navigator.userAgent.match(/BlackBerry/i) ||
-            navigator.userAgent.match(/iPhone|iPad|iPod/i) ||
-            navigator.userAgent.match(/Opera Mini/i) ||
-            navigator.userAgent.match(/IEMobile/i)
-        ) ? true : false);
-    }
 
     // handle on page scroll
     var handleHeaderOnScroll = function() {
